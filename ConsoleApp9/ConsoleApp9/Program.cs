48cd696a4602f94c59a7f78696c03277775ceae8
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-09-21 11:05:30 +0400

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap memoryImage;
+
+            while (true)
+            {
+                Console.WriteLine("Press Enter for Screenshot");
+
+                var key = Console.ReadKey(true);
+
+                if (key.Key != ConsoleKey.Enter)
+                {
+                    continue;
+                }
+                memoryImage = new Bitmap(2200, 1200);
+
+                Size size = new Size(memoryImage.Width, memoryImage.Height);
+
+                Directory.CreateDirectory($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\Screenshots");
+                using Graphics graphics = Graphics.FromImage(memoryImage);
+
+                graphics.CopyFromScreen(0, 0, 0, 0, size);
+
+                string fileName = string.Format(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\Screenshots" + @"\Screenshot" + "_" + DateTime.Now.ToString("(dd_MMMM_hh_mm_ss_tt)") + ".png");
+
+                memoryImage.Save(fileName);
+
+                Console.WriteLine("Picture saved");
+            }
+        }
+    }
+}
